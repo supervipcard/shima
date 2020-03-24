14e85aeeaafc2d4df5115c3f11a03850cb267fe3
xiangchen <805071841@qq.com>
2020-03-24 17:21:26 +0800

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py makemigrations &&
 python manage.py migrate &&
-python manage.py collectstatic &&
+python manage.py collectstatic --noinput &&
 gunicorn --config gunicorn.conf shima.wsgi:application
