e45f040ed7f65ca5e43a1f6594285d515e9ebc88
xiangchen <805071841@qq.com>
2020-03-24 17:58:11 +0800

    update

--- shima/settings.py
+++ shima/settings.py
@@ -143,10 +143,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/backend/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.UserProfile'
