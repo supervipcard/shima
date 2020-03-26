97077d20806a08db5590b9d844afd135d67d0e76
xiangchen <805071841@qq.com>
2020-03-26 15:09:21 +0800

    update

--- apps/users/views.py
+++ apps/users/views.py
@@ -20,9 +20,6 @@ from .serializers import SMSCodeSerializer, UserRegSerializer, UserDetailSeriali
 User = get_user_model()
 redis_client = get_redis_connection("default")
 
-import logging
-logger = logging.getLogger('django')
-
 
 class CustomBackend(ModelBackend):
     """
@@ -122,10 +119,6 @@ class Captcha(views.APIView):
                 'V', 'W', 'X', 'Y', 'Z']
 
     def post(self, request):
-        print('1')
-        logger.info('2')
-        logger.error('3')
-        1/0
         code_list = random.choices(self.number + self.alphabet + self.ALPHABET, k=4)
         code_text = ''.join(code_list)
         image = ImageCaptcha()
